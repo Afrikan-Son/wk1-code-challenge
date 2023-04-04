9d5d63863069fc554d4355cb7091d6ddf61aa9e0
Mark Wanjau <theafrikanson@gmail.com>
2023-04-05 02:35:50 +0300

    change whole code of html, css and javascript to suit deployment

---
+++ index1.js
@@ -0,0 +1,23 @@
+//Grade Generator
+function generateGrade() {
+    const marksInput = document.getElementById("marks");
+    const gradeOutput = document.getElementById("grade");
+    const marks = parseInt(marksInput.value);
+
+    if (isNaN(marks) || marks < 0 || marks > 100) {
+        gradeOutput.textContent = "Please enter a number between 0 and 100";
+        return;
+    }
+
+    if (marks > 79) {
+        gradeOutput.textContent = "A";
+    } else if (marks >= 60) {
+        gradeOutput.textContent = "B";
+    } else if (marks >= 50) {
+        gradeOutput.textContent = "C";
+    } else if (marks >= 40) {
+        gradeOutput.textContent = "D";
+    } else {
+        gradeOutput.textContent = "E";
+    }
+}
---
+++ index2.js
@@ -0,0 +1,19 @@
+function calculate() {
+    const speed = document.getElementById('speed').value;
+    if (speed === "") {
+        document.getElementById('result').innerHTML = "Please enter a number";
+    } else {
+        if (speed < 70) {
+            document.getElementById('result').innerHTML = "Ok";
+            document.getElementById('points').innerHTML = 0;
+        } else if (speed >= 70 && speed <= 130) {
+            let balance = speed - 70;
+            let points = Math.floor(balance / 5);
+            document.getElementById('result').innerHTML = "Ok";
+            document.getElementById('points').innerHTML = points;
+        } else {
+            document.getElementById('result').innerHTML = "License suspended";
+            document.getElementById('points').innerHTML = "";
+        }
+    }
+}
